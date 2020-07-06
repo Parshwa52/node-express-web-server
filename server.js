1996215d0b1ad3ea0d7c8845d1032b37c7eadfa3
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-06 21:31:58 +0530

    unsolved heroku deploy

--- server.js
+++ server.js
@@ -1,59 +1,60 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+const port=process.env.PORT || 3000;
+var app=express();
 
-//const port = process.env.PORT;
+hbs.registerPartials(__dirname+'/views/partials');
 
-var app = express();
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
 
-hbs.registerPartials(__dirname + '/views/partials')
+
+hbs.registerHelper('scream',(text)=>{
+return text.toLowerCase();
+});
+app.set('views', (__dirname+'/views'));
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
 
-  console.log(log);
-  fs.appendFileSync('server.log',log+'\n',(err)=>{
-    console.log('unable to log');
-})
-  next();
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log+'\n',(err)=>{
+        console.log('unable to log');
+    })
+    next();
+});
+/*app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+    next();
+});*/
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+   /* res.send({
+        name:'Parshwa',
+        likes:['chess','programming']
+    });*/
+    res.render('home.hbs',{
+        title:'Home',
+        body:'HELLO WORLD',
+        
+    });
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
 
-app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'About',
+        
+    });
+    //console.log(req);
 });
 
-hbs.registerHelper('scream', (text) => {
-  return text.toUpperCase();
-});
-
-app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-  });
-});
-
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
-
-app.listen(process.env.port || 3000, () => {
-  console.log(`Server is up on port`);
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
 });
