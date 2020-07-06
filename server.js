3f45e9fba616bcf12c75f7ad89e7f67868e6cfd9
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-06 14:00:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('scream',(text)=>{
+return text.toLowerCase();
+});
+app.set('views', (__dirname+'/views'));
+app.set('view engine', 'hbs');
+
+
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log+'\n',(err)=>{
+        console.log('unable to log');
+    })
+    next();
+});
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+    next();
+});
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+   /* res.send({
+        name:'Parshwa',
+        likes:['chess','programming']
+    });*/
+    res.render('home.hbs',{
+        title:'Home',
+        body:'HELLO WORLD',
+        
+    });
+});
+
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'About',
+        
+    });
+    //console.log(req);
+});
+
+app.listen(3000,()=>{
+    console.log('server is up');
+});
