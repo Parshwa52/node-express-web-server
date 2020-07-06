d1bd604520c1dd1dad7bfb32bface0c114642931
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-06 21:46:56 +0530

    solved heroku deployment

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/',(req,res)=>{
     });
 });
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        title: 'Projects'
+    });
+});
+
 
 
 app.get('/about',(req,res)=>{
